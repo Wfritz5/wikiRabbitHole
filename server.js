ab5f320d26000c93d8bc7812830f03fe620e0cba
Will <wfritz5@gmail.com>
2019-08-03 12:43:35 -0600

    updating 3001 connection, adding file structure

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const routes = require("./routes");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
+mongoose.connect(
+  // process.env.MONGODB_URI ||
+  "mongodb://localhost/wikidatabase", {
+    useNewUrlParser: true
+  });
 // Define API routes here
 
 // Send every other request to the React app
+
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
