23675c8efbfd95119bd88df2a5bac409bbb3b554
Will <wfritz5@gmail.com>
2019-08-03 14:02:46 -0600

    cleanup code

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-// Matches with "/api/books"
 router.route("/")
     .get(userController.findAll)
     .post(userController.create);
 
-// Matches with "/api/books/:id"
 router
     .route("/:id")
     .get(userController.findById)
