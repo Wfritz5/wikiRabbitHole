5b15d6ce40d521135dd58119a5abc8bb3666bfd4
Will <wfritz5@gmail.com>
2019-08-03 10:49:09 -0600

    file structure MERN

---
+++ client/build/precache-manifest.8898eb4e16470099739373c78143b3fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d647506daa5a7de02afb",
+    "url": "/static/js/main.2296c533.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d647506daa5a7de02afb",
+    "url": "/static/css/main.bb215dc8.chunk.css"
+  },
+  {
+    "revision": "5e9940b06541c008921a35d883dfe75f",
+    "url": "/index.html"
+  }
+];
