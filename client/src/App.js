f1fa66efbe4e30718658cf340f03a6dc35fbb8ac
RC Cola <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-08-04 20:20:50 -0600

    styled some buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Searchbar from "./components/Searchbar"
+import SearchForm from "./components/SearchForm"
+import Button from "./components/Button";
+
+const redButton = {
+  backgroundColor: "red",
+  padding: "5px",
+  margin: "5px",
+  borderRadius: "5px",
+  borderColor: "black",
+  color: "black"
+};
+
 
 class App extends Component {
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // const url = `https://en.wikipedia.org/wiki/${this.state.term}`
+    // alert(`The url is ` + url)
+    // window.open(url)
+    // this.setState({ term: ""})
+    alert("random")
+};
+
   render() {
     return (
       <div className="App">
@@ -11,7 +32,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Wiki Rabbit Hole</h2>
         </div>
-        <Searchbar />
+        <SearchForm />
+        {/* This button will pull random wikipedia articles */}
+        <Button 
+        id={"random-search"}
+        label={"Random"}
+        onClick={this.handleSubmit}
+        type={"submit"}
+        style={redButton}
+        />
       </div>
     );
   }
