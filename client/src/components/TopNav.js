2d3c92dd3de30eb1ee2a1c887e9de562b8474add
Will <wfritz5@gmail.com>
2019-08-06 08:38:22 -0600

    debugged isLoggedIn to scrape

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,6 +1,4 @@
-import React, {
-    Component
-} from "react";
+import React, { Component } from "react";
 import API from "../utils/API";
 import {
     Collapse,
@@ -54,73 +52,46 @@ export default class Navigation extends Component {
     }
 
     render() {
-        return ( <
-            div >
-            <
-            Navbar className = "navbar"
-            light expand = "md" >
-            <
-            NavbarBrand href = "/"
-            className = "titleFont" > < i className = "fas fa-key" > < /i> React Auth</NavbarBrand >
-            <
-            NavbarToggler onClick = {
-                this.toggle
-            }
-            /> <
-            Collapse isOpen = {
-                this.state.isOpen
-            }
-            navbar >
-            <
-            Nav className = "ml-auto"
-            navbar >
-            <
-            NavItem >
-            <
-            NavLink href = "/" > < i className = "fas fa-home light-text" > < /i></NavLink >
-            <
-            /NavItem> <
-            UncontrolledDropdown nav inNavbar >
-            <
-            DropdownToggle nav caret >
-            <
-            i className = "fas fa-user light-text" > < /i> <
-            /DropdownToggle> <
-            DropdownMenu right > {
-                this.state.loggedIn ? ( <
-                    >
-                    <
-                    DropdownItem >
-                    <
-                    NavLink href = "/profile" > Profile < /NavLink> <
-                    /DropdownItem> <
-                    DropdownItem >
-                    <
-                    NavLink onClick = {
-                        this.logout
-                    } > Logout < /NavLink> <
-                    /DropdownItem> <
-                    />
-                ) : ( <
-                    >
-                    <
-                    DropdownItem >
-                    <
-                    NavLink href = "/login" > login < /NavLink> <
-                    /DropdownItem> <
-                    DropdownItem >
-                    <
-                    NavLink href = "/signup" > signup < /NavLink> <
-                    /DropdownItem> <
-                    />
-                )
-            } <
-            /DropdownMenu> <
-            /UncontrolledDropdown> <
-            /Nav> <
-            /Collapse> <
-            /Navbar> <
-            /div>
+        return (
+            <div>
+                <Navbar className="navbar" light expand="md">
+                    <NavbarBrand href="/" className="titleFont"><i className="fas fa-key"></i> React Auth</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/"><i className="fas fa-home light-text"></i></NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    <i className="fas fa-user light-text"></i>
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    {this.state.loggedIn ? (
+                                        <>
+                                            <DropdownItem>
+                                                <NavLink href="/profile">Profile</NavLink>
+                                            </DropdownItem>
+                                            <DropdownItem>
+                                                <NavLink onClick={this.logout}>Logout</NavLink>
+                                            </DropdownItem>
+                                        </>
+                                    ) : (
+                                            <>
+                                                <DropdownItem>
+                                                    <NavLink href="/login">login</NavLink>
+                                                </DropdownItem>
+                                                <DropdownItem>
+                                                    <NavLink href="/signup">signup</NavLink>
+                                                </DropdownItem>
+                                            </>
+                                        )}
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
         );
     }
 }
