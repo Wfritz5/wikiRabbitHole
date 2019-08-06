0c12822a08d43342532fdc689fe81b2500ab2119
Matthew McDonald <matthewmcdonald1963@gmail.com>
2019-08-05 21:35:05 -0600

    new routes and updates to routes

--- routes/api/raburl.js
+++ routes/api/raburl.js
@@ -5,8 +5,7 @@ router.route("/")
   //  .get(userController.findAll)
     .post(raburlController.create);
 
-router
-    .route("/:id")
+router.route("/:id")
     .get(raburlController.findById)  
     .put(raburlController.update)
     .delete(raburlController.remove);
