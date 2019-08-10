e72bd068b36f016d130de6aaa1257a4eabc6c7d3
pando <spreadpando@gmail.com>
2019-08-09 23:13:58 -0600

    buttons svgs

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
 import NoMatch from "./pages/NoMatch";
 import TopNav from "./components/TopNav";
-import { Container } from 'reactstrap';
 
 const redButton = {
   backgroundColor: "red",
@@ -39,33 +38,22 @@ class App extends Component {
     return (
       <Router>
         <TopNav />
-        <Container>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
-            <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
-            <Route exact path="/profile" component={Profile} />
-            <Route component={NoMatch} />
-
-            //*Button?
-            <div className="App">
-              <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Wiki Rabbit Hole</h2>
-              </div>
-              {/* This button will pull random wikipedia articles */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
+          <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
+          <Route exact path="/profile" component={Profile} />
+          <Route component={NoMatch} />
+          //*Button?
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Wiki Rabbit Hole</h2>
             </div>
-          </Switch>
-          <SearchForm />
-          <Button
-            id={"random-search"}
-            label={"Random"}
-            onClick={this.handleSubmit}
-            type={"submit"}
-            style={redButton}
-          //*end Button?
-          />
-        </Container>
+            {/* This button will pull random wikipedia articles */}
+          </div>
+        </Switch>
+        <SearchForm />
       </Router>
     );
   }
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import Button from "./Button"
 import ScrapeButton from "./ScrapeButton"
 import styled from "styled-components";
+import randButton from "../random.png";
+
+const Random = styled.button`
+background: url('${randButton}') no-repeat;
+background-size:contain;
+height:40px;
+width:40px;
+border:0;
+margin:2px 10px 0px 10px;`;
 
 const Input = styled.input`
-border-radius: 3px;
-margin: 0.5em;
+border: 1px solid black;
+padding:5px;
+border-radius: 15px;
+height:10px;
 `;
 
 const Form = styled.form`
-padding: 0.5em;
+display:flex;
 margin: 0.5em;
 border: none;
 border-radius: 3px;
 `
 
-const redButton = {
-    backgroundColor: "red",
-    padding: "5px",
-    margin: "5px",
-    borderRadius: "5px",
-    borderColor: "black",
-    color: "black"
-  };
+const Clear = styled.button`
+background-color:#FFFFFF;
+border: 1px solid black;
+border-radius:15px;
+height:24px;
+`;
 
-const blueButton = {
-    backgroundColor: "blue",
-    padding: "5px",
-    margin: "5px",
-    borderRadius: "5px",
-    borderColor: "black",
-    color: "white"
-  };  
+const Container = styled.div`
+width:100%;
+align-content:center;
+justify-content:center;
+display:flex;
+`
 
 class SearchForm extends Component {
     state = {
@@ -40,41 +49,43 @@ class SearchForm extends Component {
 
     handleInputChange = event => {
         // update state of term
-        this.setState({ term: event.target.value})
+        this.setState({
+            term: event.target.value
+        })
     }
 
     handleClear = event => {
         event.preventDefault();
-        this.setState({ term: ""})
+        this.setState({
+            term: ""
+        })
     };
 
     render() {
         return (
-            <div>
-            <Form id="wiki-form" onSubmit={event => event.preventDefault()}>
-                <label htmlFor="wikiSearch">Wikipedia Search</label>
-                <br />
-                <Input    
-                    type="text" 
-                    name="wikiSearch" 
-                    id="wikiSearch" 
-                    placeholder="Search for an article" 
-                    value={this.state.term}
-                    onChange={this.handleInputChange}
-                />
-                {/* This button will submit the form */}
-                <ScrapeButton term={this.state.term}
-                />
-                {/* This button will clear the search field */}
-                  <Button 
-                  id={"clear-search"}
-                  label={"Clear"}
-                  onClick={() => this.setState({ term: ""})}
-                  type={"reset"}
-                  style={blueButton}
-                  />
-            </Form>
-            </div>
+            <Container >
+                <Form id="wiki-form" onSubmit={event => event.preventDefault()} >
+                    {/* <label htmlFor="wikiSearch"> Wikipedia Search </label> */}
+                    <Random
+                        id={"random-search"}
+                        label={"Random"}
+                        onClick={this.handleSubmit}
+                        type={"submit"} />
+                    <Input type="text"
+                        name="wikiSearch"
+                        id="wikiSearch"
+                        placeholder="Search for an article"
+                        value={this.state.term} onChange={this.handleInputChange} />
+                    {
+                        /* This button will submit the form */
+                    }
+                    <ScrapeButton term={this.state.term} />
+                    {
+                        /* This button will clear the search field */
+                    }
+                    <Clear id={"clear-search"} label={"Clear"} onClick={() => this.setState({ term: "" })} type={"reset"}>clear</Clear>
+                </Form >
+            </Container>
         );
     }
 }
