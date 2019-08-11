5fc5cdc3fe3eb725fc5841f5a2dcf39000fb72f2
RC Cola <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-08-11 14:23:38 -0600

    Now is filtering links to grab true wiki pages, and then generates 5 unique random links and concats wikipedia.org to the front to create 5 unique working wiki links

--- client/src/utils/scrape.js
+++ client/src/utils/scrape.js
@@ -2,50 +2,64 @@ import noImage from "../assets/noImage.svg"
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-export default function scrape (url) {
-    // this.setState({term: this.state.term})
-    // window.open(url)
-    axios.get(url).then(response => {
-      const result = {};
-      const linkArr = [];
-      const $ = cheerio.load(response.data);
-      $(".mw-body").each(function (i, element) {
-        result.title = $(this).children("h1#firstHeading").text();
-        // const title = result.title;
-        // if (title.search("disambiguation")) {
-        // result.summary = $(this).find("p").text();
-        result.image = $(this).find(".image").attr("href");
-        result.links = $(this).find("a");
-        $(result.links).each(function(i, link){
-          linkArr.push($(link).attr('href'))
-        });
-        linkArr.splice(0, 3)
-        console.log(linkArr)
-        result.url = `https://en.wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`;
-        if (result.image){
-          result.image = `https://commons.wikimedia.org${result.image}`
-          axios.get(result.image).then(function (response) {
-            const $ = cheerio.load(response.data);
-            $(".fullImageLink").each(function (i, element) {
-              result.image = $(this).children("a").attr("href")
-              console.log(result)
-              return result;
-            });
-          });
-        } else {
-          result.image = noImage;
-          console.log("Image not found")
-          console.log(result)
-          return result
+export default function scrape(url) {
+  axios.get(url).then(response => {
+    const result = {};
+    const linkArr = [];
+    const $ = cheerio.load(response.data);
+    $(".mw-body").each(function (i, element) {
+      function randomLinkGenerator(links) {
+        const arr = []
+        while (arr.length < 5) {
+          const r = Math.floor(Math.random() * links.length);
+          if (arr.indexOf(r) === -1) {
+          const randLink = `wikipedia.org${links[r]}`
+          arr.push(randLink);
+          }
         }
-        // } else {
-        //     console.log("Disambiguation")
-        // }
+        return arr;
+      }
+      result.title = $(this).children("h1#firstHeading").text();
+      // const title = result.title;
+      // if (title.search("disambiguation")) {
+      // result.summary = $(this).find("p").text();
+      result.image = $(this).find(".image").attr("href");
+      result.links = $(this).find("a");
+      result.url = `https://wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`;
+      // push all links into an array
+      $(result.links).each(function (i, link) {
+        linkArr.push(`${$(link).attr('href')}`)
       });
-    })
-    // .then((result) => {
-    // console.log(result)
+      // filter the links to grab good urls
+      result.filteredLinks = linkArr.filter(link => link.includes(`/wiki/`) && !link.includes(`:`) &&
+        !link.includes(`%`))
+      result.randomLinks = randomLinkGenerator(result.filteredLinks);
+      // checks for a good image and then will grab its base code
+      if (result.image) {
+        // need to check for `https://en.wikimedia.org${result.image}` as well
+        result.image = `https://commons.wikimedia.org${result.image}`
+        axios.get(result.image).then(function (response) {
+          const $ = cheerio.load(response.data);
+          $(".fullImageLink").each(function (i, element) {
+            result.image = $(this).children("a").attr("href")
+            console.log(result)
+            return result;
+          });
+        });
+      } else {
+        result.image = noImage;
+        console.log("Image not found")
+        console.log(result)
+        return result
+      }
+      // } else {
+      //     console.log("Disambiguation")
+      // }
+    });
+  })
+  // .then((result) => {
+  // console.log(result)
 
-    // return (result)
-    // });
-  }
+  // return (result)
+  // });
+}
